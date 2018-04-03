1715bf1ff81314d8ca9fdaeb0912b04ed85998d0
Igor Zamozhnii <zamozhnii@gmail.com>
2018-04-04 02:45:03 +0300

    end

--- app/js/main.js
+++ app/js/main.js
@@ -10,38 +10,38 @@ function init() {
     const btnsAlphabet = d.querySelectorAll('#alpha');
     const hearts = d.querySelectorAll('#heart');
 
-    slider.addEventListener('click', (e) => {
+    slider.addEventListener('click', e => {
         let target = e.target;
-        if(target.classList.contains('right')) {
-            for(let i = 0; i < list.length; i++) {
-                if(list[i].classList.contains('active')) {
+        if (target.classList.contains('right')) {
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].classList.contains('active')) {
                     let active = list[i];
                     active.classList.remove('active');
                     let nextActive = active.nextElementSibling;
-                    if(nextActive === null) {
-                        list[list.length-1].classList.remove('active');
+                    if (nextActive === null) {
+                        list[list.length - 1].classList.remove('active');
                         list[0].classList.add('active');
                         break;
-                    } 
-                    if(nextActive !== null) {
+                    }
+                    if (nextActive !== null) {
                         nextActive.classList.add('active');
                         break;
                     }
                 }
             }
         }
-        if(target.classList.contains('left')) {
-            for(let i = 0; i < list.length; i++) {
-                if(list[i].classList.contains('active')) {
+        if (target.classList.contains('left')) {
+            for (let i = 0; i < list.length; i++) {
+                if (list[i].classList.contains('active')) {
                     let active = list[i];
                     active.classList.remove('active');
                     let prevActive = active.previousElementSibling;
-                    if(prevActive === null) {
+                    if (prevActive === null) {
                         list[0].classList.remove('active');
-                        list[list.length-1].classList.add('active');
+                        list[list.length - 1].classList.add('active');
                         break;
-                    } 
-                    if(prevActive !== null) {
+                    }
+                    if (prevActive !== null) {
                         prevActive.classList.add('active');
                         break;
                     }
@@ -49,15 +49,15 @@ function init() {
             }
         }
     });
-    
-    divAlphabet.addEventListener('click', (e) => {
+
+    divAlphabet.addEventListener('click', e => {
         let target = e.target;
-        if(target.tagName === 'BUTTON') {
-            if(target.classList.contains('btn-active')) {
+        if (target.tagName === 'BUTTON') {
+            if (target.classList.contains('btn-active')) {
                 return;
             } else {
-                for(let i = 0; i < btnsAlphabet.length; i++) {
-                    if(btnsAlphabet[i].classList.contains('btn-active')) {
+                for (let i = 0; i < btnsAlphabet.length; i++) {
+                    if (btnsAlphabet[i].classList.contains('btn-active')) {
                         btnsAlphabet[i].classList.remove('btn-active');
                         break;
                     }
